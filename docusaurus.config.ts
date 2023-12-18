4dabc8a758b26854f0b5c2bc95531494e0db3425
Cardinal156 <2027156@scprep.org>
2023-12-18 17:35:37 -0500

    add(): dotenv

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,9 @@
 import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+
 import type * as Preset from '@docusaurus/preset-classic';
+import type {Config} from '@docusaurus/types';
+
+import "dotenv/config";
 
 const config: Config = {
   title: 'Cardinal Robotics',
