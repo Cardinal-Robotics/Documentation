2580bcd9a48cca495d3f48811c9f02a6dc5e6a81
Cardinal156 <2027156@scprep.org>
2024-01-20 10:24:49 -0500

    fix(): Move everything out of the all folder into their own categories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/all">
+            to="/docs/category/programming">
             Get Started
           </Link>
         </div>
